378c69fd76cd7c13d4feb6f7aefc7fb4c4f7c91b
UltimatePractitioner <92314109+UltimatePractitioner@users.noreply.github.com>
2022-03-26 20:36:31 -0400

    GIT HUB IS HARD I MADE SOME CHANGES HOLY FUCK

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x2B2b8D8A705B529E4BF67d252295D57D2C4B878e";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-gamma-six.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
--- my-app/constants/index.js
+++ my-app/constants/index.js
@@ -1752,6 +1752,6 @@ export const abi = [
 export const NFT_CONTRACT_ADDRESS =
 
 
-    "0x6805a66179283Fa1FcEc8D89f4e162bebe5C0617";
+    "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 
